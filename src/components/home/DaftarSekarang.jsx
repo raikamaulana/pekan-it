8c3b8667dd35424866d69f8964178640b3a6eff9
unknown <raikamaulana40@gmail.com>
2025-05-29 15:54:34 +0700

    Update sedikit

--- src/components/home/DaftarSekarang.jsx
+++ src/components/home/DaftarSekarang.jsx
@@ -12,11 +12,11 @@ export default function DaftarSekarang() {
                 <h1 className="w-fit inline selection:bg-purple-900 selection:text-white font-semibold text-7xl bg-gradient-to-r from-[#402DAE] to-[#BD63D1] bg-clip-text text-transparent drop-shadow-text"> Sekarang!</h1>
                 <p className='w-full max-w-3xl mx-auto selection:bg-purple-900 text-white text-center text-2xl mt-8'>Upgrade softskill dan hardskill kamu dengan berbagai rangkaian kegiatan kami!</p>
             </div>
-            <div className="mt-24 grid md:grid-cols-2 grid-cols-1 mx-auto max-w-4xl gap-5">
-                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg">Lomba UI/UX & Software Development</div>
-                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg">Lomba Competitive Programming</div>
-                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg">Webinar</div>
-                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg">Seminar & Workshop</div>
+            <div className="mt-24 grid md:grid-cols-2 grid-cols-1 mx-auto max-w-4xl gap-10">
+                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg box-shadow-card">Lomba UI/UX & Software Development</div>
+                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg box-shadow-card">Lomba Competitive Programming</div>
+                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg box-shadow-card">Webinar</div>
+                <div className="card-daftar bg-black/50 text-white text-center text-3xl h-full rounded-[65px] min-h-[401px] w-full max-w-lg box-shadow-card">Seminar & Workshop</div>
             </div>
             <div className="absolute -bottom-25 left-10 w-[1px] h-[1px] rounded-full bg-red-500 shadow-purple-glow z-1"></div>
         </section>
